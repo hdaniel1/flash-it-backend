7767086b7d2fad4a8cb6cbe48e31d405b2fc7c45
Daniel Hallada <hallada.daniel@gmail.com>
2019-08-06 17:23:25 -0400

    login finished

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create
-        @user = User.find_by(username: user_login_params[:username])
-
+        @user = Api::V1::User.find_by(username: user_login_params[:username])
+        byebug
         if @user && @user.authenticate(user_login_params[:password])
             token = encode_token({ user_id: @user.id })
-            render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+            render json: { user: Api::V1::UserSerializer.new(@user), jwt: token }, status: :accepted
         else
             render json: { message: 'Invalid username or password' }, status: :unauthorized
         end
