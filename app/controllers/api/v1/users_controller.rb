29688c178d776fb746e994b23ad41323bc07f8cd
Daniel Hallada <hallada.daniel@gmail.com>
2019-08-06 15:52:25 -0400

    create users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::UsersController < ApplicationController
     
     def create 
         @user = Api::V1::User.create(user_params)
-
+        
         if @user.valid?
             @token = encode_token(user_id: @user.id)
             render json: { user: Api::V1::UserSerializer.new(@user), jwt: @token }, status: :created
